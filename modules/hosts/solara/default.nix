b7baa921ccd3fa0e7b95d66f5cf5de199d1c088a
PadowYT2 <me@padow.ru>
2026-05-18 17:58:05 +0300

    init helius and solara

---
+++ modules/hosts/solara/default.nix
@@ -0,0 +1,79 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosConfigurations.solara = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = {inherit inputs;};
+    modules = with self.nixosModules;
+      [
+        common
+        remotebuild
+        "${inputs.nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+      ]
+      ++ map (name: ./_modules + "/${name}") (builtins.attrNames (builtins.readDir ./_modules))
+      ++ [
+        {
+          system.stateVersion = "26.05";
+
+          boot = {
+            initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "ahci" "sd_mod" "sr_mod" "virtio_blk"];
+            kernelModules = ["kvm-intel"];
+          };
+
+          disko.devices = {
+            disk.main = {
+              device = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
+              type = "disk";
+              content = {
+                type = "gpt";
+                partitions = {
+                  boot = {
+                    size = "1M";
+                    type = "EF02";
+                  };
+
+                  ESP = {
+                    size = "1G";
+                    type = "EF00";
+                    content = {
+                      type = "filesystem";
+                      format = "vfat";
+                      mountpoint = "/boot";
+                      mountOptions = ["umask=0077"];
+                    };
+                  };
+
+                  root = {
+                    size = "100%";
+                    content = {
+                      type = "filesystem";
+                      format = "ext4";
+                      mountpoint = "/";
+                    };
+                  };
+                };
+              };
+            };
+          };
+
+          networking.hostName = "solara";
+
+          systemd.network.networks."10-ens3" = {
+            matchConfig.Name = "ens3";
+            address = ["143.20.79.251" "2a12:bec4:1821:1b7::a/64"];
+            gateway = ["143.20.79.1"];
+            routes = [
+              {
+                Destination = "::/0";
+                Gateway = "2a12:bec4:1821::";
+                GatewayOnLink = true;
+                Metric = 1024;
+              }
+            ];
+            linkConfig.RequiredForOnline = "routable";
+          };
+        }
+      ];
+  };
+}
