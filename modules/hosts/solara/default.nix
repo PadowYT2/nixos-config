91af53db343986ad4d3bde9d722ae2692e5eb073
PadowYT2 <me@padow.ru>
2026-05-19 20:49:26 +0300

    add kernel.sysctl to solara and helius

--- modules/hosts/solara/default.nix
+++ modules/hosts/solara/default.nix
@@ -19,6 +19,12 @@
           boot = {
             initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "ahci" "sd_mod" "sr_mod" "virtio_blk"];
             kernelModules = ["kvm-intel"];
+            kernel.sysctl = {
+              "net.core.default_qdisc" = "fq";
+              "net.ipv4.tcp_congestion_control" = "bbr";
+              "net.ipv4.tcp_fastopen" = 3;
+              "net.ipv4.tcp_mtu_probing" = 1;
+            };
           };
 
           disko.devices = {
