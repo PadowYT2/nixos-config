fb0bc1ba43810f1fadc18e30e7f89e236a60c006
PadowYT2 <me@padow.ru>
2026-05-26 11:32:21 +0300

    add beszel hub

---
+++ modules/hosts/noctis/_modules/beszel/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  inputs,
+  ...
+}: {
+  services.caddy.virtualHosts = {
+    "uptime.proxied.host".extraConfig = ''
+      reverse_proxy http://localhost:8090
+    '';
+  };
+
+  services.beszel.hub = {
+    enable = true;
+    environment = {
+      APP_URL = "https://uptime.proxied.host";
+    };
+  };
+}
