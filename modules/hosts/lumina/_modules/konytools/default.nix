4b7a51d29edd6b83461b3460b9cf80f135b79357
PadowYT2 <me@padow.ru>
2026-05-28 20:59:25 +0000

    update konytools

--- modules/hosts/lumina/_modules/konytools/default.nix
+++ modules/hosts/lumina/_modules/konytools/default.nix
@@ -11,11 +11,19 @@
     clientId = "1258371284858044456";
     discordTokenFile = config.age.secrets."konytools.discord".path;
     geminiKeyFile = config.age.secrets."konytools.gemini".path;
+    cloudflareAccountId = "20c1841ca5455cea9d680f9e6f138f24";
+    cloudflareKeyFile = config.age.secrets."konytools.cloudflare".path;
   };
 
   age.secrets = {
     "konytools.discord" = {
       file = secrets/discord.age;
+      owner = "konytools";
+      group = "konytools";
+    };
+
+    "konytools.cloudflare" = {
+      file = secrets/cloudflare.age;
       owner = "konytools";
       group = "konytools";
     };
