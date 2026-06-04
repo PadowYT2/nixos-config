9f90daa71f45322b650d939a83b63713892edd52
PadowYT2 <me@padow.ru>
2026-06-04 16:40:02 +0300

    improve easyeffects and fix pipewire

--- modules/hosts/padow/_modules/pipewire/default.nix
+++ modules/hosts/padow/_modules/pipewire/default.nix
@@ -5,5 +5,16 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable = true;
+
+    extraConfig.pipewire."92-audio-stability" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [44100 48000];
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 512;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
   };
 }
