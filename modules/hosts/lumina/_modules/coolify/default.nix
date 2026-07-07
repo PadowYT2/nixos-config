b5a38f0a34ebcddc4fe7a1f2d6e00face13caea2
PadowYT2 <me@padow.ru>
2026-07-07 20:09:34 +0300

    update caddy hash

--- modules/hosts/lumina/_modules/coolify/default.nix
+++ modules/hosts/lumina/_modules/coolify/default.nix
@@ -6,7 +6,7 @@
   services.caddy = {
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.2.4"];
-      hash = "sha256-bzMqxWTqrJ1skZmRTXyEMCKStXpljbqe5r0Ve2cnBfM=";
+      hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
     };
 
     virtualHosts = {
