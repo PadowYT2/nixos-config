780078eaf5606332ddf3b910534271493b049283
PadowYT2 <me@padow.ru>
2026-07-17 11:46:22 +0300

    add alsa-tools to vespera

---
+++ modules/hosts/vespera/_modules/pipewire/default.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+
+    extraConfig.pipewire."92-audio-stability" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [48000];
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 1024;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ncpamixer alsa-tools];
+}
