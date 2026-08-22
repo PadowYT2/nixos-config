2527f01525e8df6078e6c74155f9b3a9cfcb9054
PadowYT2 <me@padow.dev>
2026-08-22 04:17:47 +0300

    directly backup via cifs instead of having a hop

--- modules/hosts/lumina/_modules/restic/default.nix
+++ modules/hosts/lumina/_modules/restic/default.nix
@@ -1,93 +1,49 @@
 {config, ...}: {
   services.restic = {
-    server = {
-      enable = true;
-      listenAddress = "8200";
-      dataDir = "/srv/backups";
-      privateRepos = true;
-      htpasswd-file = config.age.secrets."restic.htpasswd".path;
-    };
+    backups.lumina = {
+      repository = "/mnt/storage-box/lumina";
+      passwordFile = config.age.secrets."restic.password".path;
+      environmentFile = config.age.secrets."restic.environment".path;
 
-    backups = {
-      lumina-local = {
-        repository = "rest:http://localhost:8200/lumina";
-        passwordFile = config.age.secrets."restic.password".path;
-        environmentFile = config.age.secrets."restic.environment".path;
+      paths = [
+        "/var/lib"
+        "/srv/storage"
+        "/etc"
+        "/root"
+      ];
 
-        paths = [
-          "/var/lib"
-          "/srv/storage"
-          "/etc"
-          "/root"
-        ];
+      exclude = [
+        "/var/lib/docker/overlay2/**"
+        "/var/lib/docker/image/**"
+        "/var/lib/docker/buildkit/**"
+        "/var/lib/docker/containers/**"
+        "/var/lib/docker/runtimes/**"
+        "/var/lib/systemd/**"
+        "/root/repos/**"
+      ];
 
-        exclude = [
-          "/var/lib/docker/overlay2/**"
-          "/var/lib/docker/image/**"
-          "/var/lib/docker/buildkit/**"
-          "/var/lib/docker/containers/**"
-          "/var/lib/docker/runtimes/**"
-          "/var/lib/systemd/**"
-          "/root/repos/**"
-        ];
-
-        initialize = true;
-        timerConfig = {
-          OnCalendar = "00:00";
-          Persistent = true;
-        };
-
-        pruneOpts = [
-          "--keep-daily 3"
-        ];
+      initialize = true;
+      timerConfig = {
+        OnCalendar = "01:00";
+        Persistent = true;
       };
 
-      lumina-remote = {
-        repository = "sftp://u488452-sub1@u488452-sub1.your-storagebox.de:23//home/lumina";
-        passwordFile = config.age.secrets."restic.password".path;
-        environmentFile = config.age.secrets."restic.environment".path;
-
-        paths = [
-          "/var/lib"
-          "/srv/storage"
-          "/etc"
-          "/root"
-        ];
-
-        exclude = [
-          "/var/lib/docker/overlay2/**"
-          "/var/lib/docker/image/**"
-          "/var/lib/docker/buildkit/**"
-          "/var/lib/docker/containers/**"
-          "/var/lib/docker/runtimes/**"
-          "/var/lib/systemd/**"
-          "/root/repos/**"
-        ];
-
-        initialize = true;
-        timerConfig = {
-          OnCalendar = "01:00";
-          Persistent = true;
-        };
-
-        pruneOpts = [
-          "--keep-daily 7"
-          "--keep-weekly 4"
-          "--keep-monthly 6"
-        ];
-      };
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 6"
+      ];
     };
+  };
+
+  systemd.services."restic-backups-lumina" = {
+    requires = ["mnt-storage\\x2dbox.mount"];
+    after = ["mnt-storage\\x2dbox.mount"];
   };
 
   age.secrets = {
     "restic.environment" = {
       file = secrets/environment.age;
-    };
-
-    "restic.htpasswd" = {
-      file = secrets/htpasswd.age;
-      owner = "restic";
-      group = "restic";
     };
 
     "restic.password" = {
