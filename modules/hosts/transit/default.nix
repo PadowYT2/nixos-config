9d47c7b0a8faaa6b6d014ba7ee95f0d81f70e0a1
PadowYT2 <me@padow.dev>
2026-08-25 00:15:29 +0300

    import keys for transit

--- modules/hosts/transit/default.nix
+++ modules/hosts/transit/default.nix
@@ -28,200 +28,202 @@ in {
       ]
       ++ map (name: ./_modules + "/${name}") (builtins.attrNames (builtins.readDir ./_modules))
       ++ [
-        {
-          system.stateVersion = "26.05";
+        (
+          {keys, ...}: {
+            system.stateVersion = "26.05";
 
-          boot = {
-            initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "ahci" "sr_mod" "virtio_blk"];
+            boot = {
+              initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "ahci" "sr_mod" "virtio_blk"];
 
-            kernel.sysctl = {
-              "net.core.default_qdisc" = "fq";
-              "net.ipv4.tcp_congestion_control" = "bbr";
+              kernel.sysctl = {
+                "net.core.default_qdisc" = "fq";
+                "net.ipv4.tcp_congestion_control" = "bbr";
 
-              "net.ipv4.ip_forward" = 1;
-              "net.ipv6.conf.all.forwarding" = 1;
-              "net.ipv4.tcp_syncookies" = 1;
-              "net.ipv4.tcp_max_syn_backlog" = 2048;
-              "net.netfilter.nf_conntrack_max" = 262144;
-              "net.netfilter.nf_conntrack_tcp_loose" = 0;
-              "net.ipv4.tcp_abort_on_overflow" = 1;
+                "net.ipv4.ip_forward" = 1;
+                "net.ipv6.conf.all.forwarding" = 1;
+                "net.ipv4.tcp_syncookies" = 1;
+                "net.ipv4.tcp_max_syn_backlog" = 2048;
+                "net.netfilter.nf_conntrack_max" = 262144;
+                "net.netfilter.nf_conntrack_tcp_loose" = 0;
+                "net.ipv4.tcp_abort_on_overflow" = 1;
 
-              "net.ipv4.conf.all.rp_filter" = 2;
-              "net.ipv4.conf.default.rp_filter" = 2;
-              "net.ipv4.conf.ens3.rp_filter" = 2;
-              "net.ipv4.conf.ipsec0.rp_filter" = 0;
+                "net.ipv4.conf.all.rp_filter" = 2;
+                "net.ipv4.conf.default.rp_filter" = 2;
+                "net.ipv4.conf.ens3.rp_filter" = 2;
+                "net.ipv4.conf.ipsec0.rp_filter" = 0;
+              };
             };
-          };
 
-          disko.devices = {
-            disk.main = {
-              device = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
-              type = "disk";
-              content = {
-                type = "gpt";
-                partitions = {
-                  boot = {
-                    size = "1M";
-                    type = "EF02";
-                  };
+            disko.devices = {
+              disk.main = {
+                device = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
+                type = "disk";
+                content = {
+                  type = "gpt";
+                  partitions = {
+                    boot = {
+                      size = "1M";
+                      type = "EF02";
+                    };
 
-                  ESP = {
-                    size = "1G";
-                    type = "EF00";
-                    content = {
-                      type = "filesystem";
-                      format = "vfat";
-                      mountpoint = "/boot";
-                      mountOptions = ["umask=0077"];
+                    ESP = {
+                      size = "1G";
+                      type = "EF00";
+                      content = {
+                        type = "filesystem";
+                        format = "vfat";
+                        mountpoint = "/boot";
+                        mountOptions = ["umask=0077"];
+                      };
+                    };
+
+                    root = {
+                      size = "100%";
+                      content = {
+                        type = "filesystem";
+                        format = "ext4";
+                        mountpoint = "/";
+                      };
                     };
                   };
+                };
+              };
+            };
 
-                  root = {
-                    size = "100%";
-                    content = {
-                      type = "filesystem";
-                      format = "ext4";
-                      mountpoint = "/";
-                    };
+            networking = {
+              hostName = "transit";
+
+              firewall = {
+                allowedUDPPorts = [500 4500];
+                checkReversePath = "loose";
+                trustedInterfaces = ["ipsec0"];
+
+                extraForwardRules = ''
+                  iifname "ipsec0" oifname "ens3" accept
+                  iifname "ens3" oifname "ipsec0" accept
+                '';
+              };
+
+              nftables = {
+                enable = true;
+                tables = {
+                  mss-clamp = {
+                    family = "inet";
+                    content = ''
+                      chain forward {
+                        type filter hook forward priority mangle; policy accept;
+                        tcp flags syn / syn,rst tcp option maxseg size set rt mtu
+                      }
+                    '';
+                  };
+                  transit-nat = {
+                    family = "inet";
+                    content = ''
+                      chain postrouting {
+                        type nat hook postrouting priority srcnat; policy accept;
+                        ip saddr 5.9.109.0/27 ip daddr 10.0.0.2 masquerade
+                        ip6 saddr 2a01:4f8:162:502e::/64 ip6 daddr fd00:1337::2 masquerade
+                      }
+                    '';
                   };
                 };
               };
-            };
-          };
 
-          networking = {
-            hostName = "transit";
+              iproute2.enable = true;
 
-            firewall = {
-              allowedUDPPorts = [500 4500];
-              checkReversePath = "loose";
-              trustedInterfaces = ["ipsec0"];
+              nat = {
+                enable = true;
+                enableIPv6 = true;
+                externalInterface = "ens3";
+                internalInterfaces = ["ipsec0"];
+                internalIPs = ["5.9.109.0/27" "10.0.0.0/24"];
 
-              extraForwardRules = ''
-                iifname "ipsec0" oifname "ens3" accept
-                iifname "ens3" oifname "ipsec0" accept
-              '';
-            };
-
-            nftables = {
-              enable = true;
-              tables = {
-                mss-clamp = {
-                  family = "inet";
-                  content = ''
-                    chain forward {
-                      type filter hook forward priority mangle; policy accept;
-                      tcp flags syn / syn,rst tcp option maxseg size set rt mtu
+                forwardPorts =
+                  (builtins.concatMap (port: mkForward port "tcp") [
+                    25
+                    80
+                    110
+                    143
+                    443
+                    465
+                    587
+                    993
+                    995
+                    2222
+                    4190
+                    5555
+                    20411
+                    25565
+                    25567
+                    64738
+                  ])
+                  ++ (builtins.concatMap (port: mkForward port "udp") [
+                    443
+                    54322
+                    64738
+                  ])
+                  ++ [
+                    {
+                      destination = "10.0.0.2:26000-27000";
+                      sourcePort = "26000:27000";
+                      proto = "tcp";
+                      loopbackIPs = ["95.135.208.17"];
                     }
-                  '';
-                };
-                transit-nat = {
-                  family = "inet";
-                  content = ''
-                    chain postrouting {
-                      type nat hook postrouting priority srcnat; policy accept;
-                      ip saddr 5.9.109.0/27 ip daddr 10.0.0.2 masquerade
-                      ip6 saddr 2a01:4f8:162:502e::/64 ip6 daddr fd00:1337::2 masquerade
+                    {
+                      destination = "10.0.0.2:26000-27000";
+                      sourcePort = "26000:27000";
+                      proto = "udp";
+                      loopbackIPs = ["95.135.208.17"];
                     }
-                  '';
+                  ];
+              };
+            };
+
+            systemd.network = {
+              networks = {
+                "10-ens3" = {
+                  matchConfig.Name = "ens3";
+                  address = ["95.135.208.17/24" "2a12:bec4:1821:61f::a/64"];
+                  gateway = ["95.135.208.1"];
+                  routes = [
+                    {
+                      Destination = "2a12:bec4:1821::1/128";
+                      Scope = "link";
+                    }
+                    {
+                      Destination = "::/0";
+                      Gateway = "2a12:bec4:1821::1";
+                      Metric = 1024;
+                    }
+                  ];
+                  xfrm = ["ipsec0"];
+                  linkConfig.RequiredForOnline = "routable";
+                };
+
+                "20-ipsec0" = {
+                  matchConfig.Name = "ipsec0";
+                  address = ["10.0.0.1/24" "fd00:1337::1/64"];
+                  linkConfig = {
+                    MTUBytes = "1400";
+                    RequiredForOnline = "no";
+                  };
+                };
+              };
+
+              netdevs = {
+                "20-ipsec0" = {
+                  netdevConfig = {
+                    Name = "ipsec0";
+                    Kind = "xfrm";
+                  };
+                  xfrmConfig.InterfaceId = 42;
                 };
               };
             };
 
-            iproute2.enable = true;
-
-            nat = {
-              enable = true;
-              enableIPv6 = true;
-              externalInterface = "ens3";
-              internalInterfaces = ["ipsec0"];
-              internalIPs = ["5.9.109.0/27" "10.0.0.0/24"];
-
-              forwardPorts =
-                (builtins.concatMap (port: mkForward port "tcp") [
-                  25
-                  80
-                  110
-                  143
-                  443
-                  465
-                  587
-                  993
-                  995
-                  2222
-                  4190
-                  5555
-                  20411
-                  25565
-                  25567
-                  64738
-                ])
-                ++ (builtins.concatMap (port: mkForward port "udp") [
-                  443
-                  54322
-                  64738
-                ])
-                ++ [
-                  {
-                    destination = "10.0.0.2:26000-27000";
-                    sourcePort = "26000:27000";
-                    proto = "tcp";
-                    loopbackIPs = ["95.135.208.17"];
-                  }
-                  {
-                    destination = "10.0.0.2:26000-27000";
-                    sourcePort = "26000:27000";
-                    proto = "udp";
-                    loopbackIPs = ["95.135.208.17"];
-                  }
-                ];
-            };
-          };
-
-          systemd.network = {
-            networks = {
-              "10-ens3" = {
-                matchConfig.Name = "ens3";
-                address = ["95.135.208.17/24" "2a12:bec4:1821:61f::a/64"];
-                gateway = ["95.135.208.1"];
-                routes = [
-                  {
-                    Destination = "2a12:bec4:1821::1/128";
-                    Scope = "link";
-                  }
-                  {
-                    Destination = "::/0";
-                    Gateway = "2a12:bec4:1821::1";
-                    Metric = 1024;
-                  }
-                ];
-                xfrm = ["ipsec0"];
-                linkConfig.RequiredForOnline = "routable";
-              };
-
-              "20-ipsec0" = {
-                matchConfig.Name = "ipsec0";
-                address = ["10.0.0.1/24" "fd00:1337::1/64"];
-                linkConfig = {
-                  MTUBytes = "1400";
-                  RequiredForOnline = "no";
-                };
-              };
-            };
-
-            netdevs = {
-              "20-ipsec0" = {
-                netdevConfig = {
-                  Name = "ipsec0";
-                  Kind = "xfrm";
-                };
-                xfrmConfig.InterfaceId = 42;
-              };
-            };
-          };
-
-          users.users.root.openssh.authorizedKeys.keys = with keys; [djoh];
-        }
+            users.users.root.openssh.authorizedKeys.keys = with keys; [djoh];
+          }
+        )
       ];
   };
 }
