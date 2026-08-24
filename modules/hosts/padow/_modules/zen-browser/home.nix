6b5f2c6393072a153aa2afb13b42cce116646357
PadowYT2 <me@padow.dev>
2026-08-24 18:50:22 +0300

    set a default search engine

--- modules/hosts/padow/_modules/zen-browser/home.nix
+++ modules/hosts/padow/_modules/zen-browser/home.nix
@@ -9,5 +9,12 @@
     enable = true;
     setAsDefaultBrowser = true;
     nativeMessagingHosts = [pkgs.gnome-browser-connector];
+
+    profiles.default = {
+      search = {
+        force = true;
+        default = "google";
+      };
+    };
   };
 }
