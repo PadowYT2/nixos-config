3aeb43f2f9f905d52394fa467d2b1a6a4332d567
PadowYT2 <me@padow.dev>
2026-09-21 16:34:26 +0300

    add cloudflare back

--- modules/hosts/lumina/default.nix
+++ modules/hosts/lumina/default.nix
@@ -19,7 +19,37 @@
             lib,
             keys,
             ...
-          }: {
+          }: let
+            cloudflare = {
+              ipv4 = [
+                "173.245.48.0/20"
+                "103.21.244.0/22"
+                "103.22.200.0/22"
+                "103.31.4.0/22"
+                "141.101.64.0/18"
+                "108.162.192.0/18"
+                "190.93.240.0/20"
+                "188.114.96.0/20"
+                "197.234.240.0/22"
+                "198.41.128.0/17"
+                "162.158.0.0/15"
+                "104.16.0.0/13"
+                "104.24.0.0/14"
+                "172.64.0.0/13"
+                "131.0.72.0/22"
+              ];
+
+              ipv6 = [
+                "2400:cb00::/32"
+                "2606:4700::/32"
+                "2803:f800::/32"
+                "2405:b500::/32"
+                "2405:8100::/32"
+                "2a06:98c0::/29"
+                "2c0f:f248::/32"
+              ];
+            };
+          in {
             system.stateVersion = "26.05";
 
             boot = {
@@ -187,6 +217,12 @@
             services.caddy = {
               enable = true;
               openFirewall = true;
+
+              globalConfig = ''
+                servers {
+                  trusted_proxies static private_ranges ${builtins.concatStringsSep " " (cloudflare.ipv4 ++ cloudflare.ipv6)}
+                }
+              '';
             };
 
             users.users.root.openssh.authorizedKeys.keys = with keys; [djoh];
